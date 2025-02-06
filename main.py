bd96c3ed56e62db681648f369a54dd461a352023
songya@atlasv <songya@atlasv.com>
2025-02-06 17:57:59 +0800

    端口环境变量

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, app
 
 
@@ -10,4 +12,5 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=True, host='0.0.0.0', port=port)
